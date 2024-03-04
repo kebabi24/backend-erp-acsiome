75bb84601eb31ddd00966321357d38337b109698
kamelneo <kamel.bensaoud@gmail.com>
2024-03-04 12:00:20 +0100

    change index

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -600,12 +600,12 @@ export default async ({ expressApp }) => {
     targetKey: 'tx2_tax_code',
   });
   require('../models/item').default.hasMany(require('../models/location-detail').default, {
-    foreignKey: 'ld_part',
-    sourceKey: 'pt_part',
+     foreignKey: 'ld_part',
+     sourceKey: 'pt_part',
   });
   require('../models/location-detail').default.hasOne(require('../models/item').default, {
-    foreignKey: 'ld_part',
-    targetKey: 'pt_part',
+    foreignKey: 'pt_part',
+    targetKey: 'ld_part',
   });
   require('../models/item').default.hasOne(require('../models/inventory-transaction').default, {
     foreignKey: 'tr_part',
@@ -1096,7 +1096,7 @@ export default async ({ expressApp }) => {
   });
   Logger.info('✌️ ADD MODEL ASSOCIATION');
   // sync models
-  //await sequelizeConnection.sync();
+  await sequelizeConnection.sync();
   // await sequelizeConnection
   //   .sync({ alter: true })
   //   .then(() => {
